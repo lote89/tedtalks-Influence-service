8ccd70f3f72457fbe515ca8baeba0b9c3a9ef6f2
lote89 <42542694+lote89@users.noreply.github.com>
2025-12-07 19:02:40 +0100

    Create TedTalksApplication.java

---
+++ src/main/java/com/io/ted/TedTalksApplication.java
@@ -0,0 +1,11 @@
+package com.io.ted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TedTalksApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TedTalksApplication.class, args);
+    }
+}
